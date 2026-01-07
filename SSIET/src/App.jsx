d80b5b26244034132378cc0e84bf1bd6368a1e0f
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-01-07 19:04:22 +0530

    create achievement page, with basic styling

--- SSIET/src/App.jsx
+++ SSIET/src/App.jsx
@@ -1,9 +1,11 @@
+import Achievements from "./components/Achievements";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Achievements />
+    {/* <Navbar /> */}
     </>
   );
 }
